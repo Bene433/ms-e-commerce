ade34a4224a13354a1570b028a62b1395037b4a9
bene <matheusbenediht@gmail.com>
2023-08-01 15:53:54 -0300

    Config server

---
+++ ec-config-server/src/test/java/com/cloudfy/ecommerce/ecconfigserver/EcConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudfy.ecommerce.ecconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
