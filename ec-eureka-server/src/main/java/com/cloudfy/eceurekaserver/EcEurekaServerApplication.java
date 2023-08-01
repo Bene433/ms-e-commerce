337ce21ebccbc367f4a89478f27e823897b1eaaf
bene <matheusbenediht@gmail.com>
2023-08-01 15:26:03 -0300

    First Commit

---
+++ ec-eureka-server/src/main/java/com/cloudfy/eceurekaserver/EcEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.cloudfy.eceurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcEurekaServerApplication.class, args);
+	}
+
+}
