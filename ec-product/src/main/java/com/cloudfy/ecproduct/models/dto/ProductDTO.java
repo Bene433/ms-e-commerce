bdb9f6a3776a9296f73c3046bd57d9126eb2b744
bene <matheusbenediht@gmail.com>
2023-08-05 19:02:41 -0300

    Implementando lombock e validator na entidade Product

--- ec-product/src/main/java/com/cloudfy/ecproduct/models/dto/ProductDTO.java
+++ ec-product/src/main/java/com/cloudfy/ecproduct/models/dto/ProductDTO.java
@@ -1,60 +1,22 @@
 package com.cloudfy.ecproduct.models.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductDTO {
 
     private Long id;
+    @NotBlank
     private String name;
+    @NotBlank
     private String description;
+    @NotBlank
     private Double price;
+    @NotBlank
     private Integer quantity;
-
-    public ProductDTO() {}
-
-    public ProductDTO(Long id, String name, String description, Double price, Integer quantity) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.price = price;
-        this.quantity = quantity;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
 }
