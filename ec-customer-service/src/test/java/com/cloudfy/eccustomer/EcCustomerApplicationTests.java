210305d31fbe9af025f5e3578333163779ff1c8c
bene <matheusbenediht@gmail.com>
2023-08-06 01:08:23 -0300

    Criando e realizando as configurações iniciais do serviço de cliente

---
+++ ec-customer-service/src/test/java/com/cloudfy/eccustomer/EcCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudfy.eccustomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcCustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
