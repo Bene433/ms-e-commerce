74602d532d372648c96747c3009cf5248691649e
bene <matheusbenediht@gmail.com>
2023-08-11 15:30:00 -0300

    Ec-oauth service

---
+++ ec-oauth/src/test/java/com/cloudfy/ecoauth/EcOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudfy.ecoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
