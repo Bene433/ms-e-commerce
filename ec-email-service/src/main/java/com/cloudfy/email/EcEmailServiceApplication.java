cc45651a94f91f31890b373360129d50dfd576d4
bene <matheusbenediht@gmail.com>
2023-08-17 11:24:11 -0300

    E-mail service

---
+++ ec-email-service/src/main/java/com/cloudfy/email/EcEmailServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cloudfy.email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcEmailServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcEmailServiceApplication.class, args);
+	}
+
+}
