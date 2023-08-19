ea87e87bf372b8f927fcf4a77b3aaa834d59c878
bene <matheusbenediht@gmail.com>
2023-08-19 19:51:53 -0300

    Implementando serviço de menssageria no microsserviço de e-mail

---
+++ ec-email-service/src/main/java/com/cloudfy/email/consumers/EmailConsumer.java
@@ -0,0 +1,23 @@
+package com.cloudfy.email.consumers;
+
+import com.cloudfy.email.models.dto.EmailDTO;
+import com.cloudfy.email.models.entities.EmailEntity;
+import com.cloudfy.email.services.EmailService;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailConsumer {
+
+    @Autowired
+    private EmailService emailService;
+
+    @RabbitListener(queues = "${spring.rabbitmq.queue}")
+    public void listen(@Payload EmailDTO dataDTO){
+        EmailEntity email = emailService.sendEmail(dataDTO);
+        System.out.println("Email status: " + email.getStatus().toString());
+    }
+}
